23721261e67ce53665df9dead87a6c6ecf78d3ef
Robert Klaus <r.klaus@rz.uni-kiel.de>
2023-01-26 00:11:10 +0100

    Updated Docker compose and certbot

--- scripts/renew-ssl.sh
+++ scripts/renew-ssl.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
-cd /opt/mailcow-dockerized && docker-compose down
-certbot renew --pre-hook "systemctl stop apache2" --post-hook "systemctl start apache2" --renew-hook "systemctl reload apache2" --quiet
+systemctl stop postfix@-.service
+cd /opt/mailcow-dockerized && docker compose down
+#certbot renew --quiet
+certbot renew 
 cp /etc/letsencrypt/live/DOMAIN/fullchain.pem ./data/assets/ssl/cert.pem
 cp /etc/letsencrypt/live/DOMAIN/privkey.pem ./data/assets/ssl/key.pem
-docker-compose up -d
+docker compose up -d
+systemctl start postfix@-.service
